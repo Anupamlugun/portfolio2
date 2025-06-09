7b5564c2a2d85d14cca1b7f0a656bd618efc14d2
Anupam Lugun <167012850+Anupamlugun@users.noreply.github.com>
2025-06-09 06:08:20 +0000

    vercel analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
+           <Analytics />
         </ThemeProvider>
       </body>
     </html>
