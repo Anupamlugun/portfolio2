05bacc28a2f831bb59c958f5fba61a38f1c9ebe5
Anupam Lugun <167012850+Anupamlugun@users.noreply.github.com>
2025-06-23 10:50:26 +0530

    Update Hero.js

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -7,20 +7,14 @@ export default function Hero() {
       className="relative flex h-screen items-center justify-center bg-gray-900 text-white overflow-hidden"
     >
       <div
-        className="absolute inset-0 z-0 bg-cover bg-center"
-        style={{
-          backgroundImage:
-            "url('/img/bg-mobile.jpg')",
-        }}
-      >
-        <style jsx>{`
-          @media (min-width: 1024px) {
-            div[style] {
-              background-image: url('/img/bg.png');
-            }
-          }
-        `}</style>
-      </div>
+        className="
+          absolute inset-0 z-0 
+          bg-[url('/img/bg-mobile.webp')] 
+          lg:bg-[url('/img/bg.webp')] 
+          bg-cover bg-center
+        "
+      ></div>
+
       <div className="z-10">
         <Type />
       </div>
